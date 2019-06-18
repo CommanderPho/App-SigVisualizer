95bad4cf170f5e3949752b503d061eb2a9543d48
Pho Hale <phohale@gmail.com>
2019-06-18 03:06:19 -0400

    Modifying to display a "console" like window with the raw data stream

--- ui_sigvisualizer.py
+++ ui_sigvisualizer.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'ui_sigvisualizer.ui'
+# Form implementation generated from reading ui file '/Users/pho/Downloads/App-SigVisualizer-master/ui_sigvisualizer.ui'
 #
-# Created by: PyQt5 UI code generator 5.11.2
+# Created by: PyQt5 UI code generator 5.6
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -36,15 +36,22 @@ class Ui_MainWindow(object):
         self.treeWidget.setObjectName("treeWidget")
         self.treeWidget.headerItem().setText(0, "1")
         self.gridLayout.addWidget(self.treeWidget, 0, 1, 1, 1)
-        self.widget = PaintWidget(self.centralwidget)
-        self.widget.setObjectName("widget")
-        self.gridLayout.addWidget(self.widget, 0, 2, 2, 1)
         self.updateButton = QtWidgets.QPushButton(self.centralwidget)
         self.updateButton.setObjectName("updateButton")
         self.gridLayout.addWidget(self.updateButton, 1, 1, 1, 1)
+        self.widget = PaintWidget(self.centralwidget)
+        self.widget.setObjectName("widget")
+        self.gridLayout.addWidget(self.widget, 0, 2, 1, 1)
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.btnShowDataStream = QtWidgets.QToolButton(self.centralwidget)
+        self.btnShowDataStream.setToolButtonStyle(QtCore.Qt.ToolButtonTextOnly)
+        self.btnShowDataStream.setObjectName("btnShowDataStream")
+        self.horizontalLayout.addWidget(self.btnShowDataStream)
+        self.gridLayout.addLayout(self.horizontalLayout, 1, 2, 1, 1)
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 1123, 26))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 1123, 22))
         self.menubar.setObjectName("menubar")
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
@@ -58,15 +65,6 @@ class Ui_MainWindow(object):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
         self.updateButton.setText(_translate("MainWindow", "Update Streams"))
+        self.btnShowDataStream.setText(_translate("MainWindow", "Show Received Data Stream..."))
 
 from paintwidget import PaintWidget
-
-if __name__ == "__main__":
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
-    sys.exit(app.exec_())
-
